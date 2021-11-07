2c67714b566f4155b726e8a4a684c46b62628a9b
Sergey Peleskov <info@s1temaker.ru>
2021-11-07 13:37:44 +0200

    Добавил склонение для цифр больше 20

--- lesson_1/ex_3.py
+++ lesson_1/ex_3.py
@@ -1,2 +1,5 @@
 for i in range(1, 101):
-    print(f"{i} процент{'' if i == 1 else 'а' if 1 < i < 5 else 'ов'}")
+    check_dig = i if i < 20 else int(str(i)[-1])
+    print(f"{i} процент{'' if check_dig == 1 else 'а' if 1 < check_dig < 5 else 'ов'}")
+
+
