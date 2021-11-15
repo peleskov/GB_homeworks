dbf4ffcfa7a79db901cd7b5570831679f48afc5e
Sergey Peleskov <info@s1temaker.ru>
2021-11-15 15:24:31 +0200

    Fix

--- lesson_4/Ex_2.py
+++ lesson_4/Ex_2.py
@@ -7,12 +7,8 @@ def currency_rates(currency):
     if r.status_code != 200:
         return 'Server not responding!'
     else:
-        f = open('cyrrency.xml', 'w')
-        f.write(r.text)
-        f.close()
-        tree = ET.parse('cyrrency.xml')
-        root = tree.getroot()
-        currency_info = tree.find(f"Valute[CharCode='{currency.upper()}']")
+        root = ET.fromstring(r.text)
+        currency_info = root.find(f"Valute[CharCode='{currency.upper()}']")
         if currency_info:
             return f"Курс {currency_info.find('Name').text}: {currency_info.find('Nominal').text} {currency_info.find('CharCode').text} = {Decimal(currency_info.find('Value').text.replace(',','.'))} руб"
 
--- lesson_4/Ex_3.py
+++ lesson_4/Ex_3.py
@@ -8,13 +8,9 @@ def currency_rates(currency):
     if r.status_code != 200:
         return 'Server not responding!'
     else:
-        f = open('cyrrency.xml', 'w')
-        f.write(r.text)
-        f.close()
-        tree = ET.parse('cyrrency.xml')
-        root = tree.getroot()
+        root = ET.fromstring(r.text)
         currency_date = datetime.strftime(datetime.strptime(root.get('Date'), '%d.%m.%Y'), '%d/%m/%Y')
-        currency_info = tree.find(f"Valute[CharCode='{currency.upper()}']")
+        currency_info = root.find(f"Valute[CharCode='{currency.upper()}']")
         if currency_info:
             return f"Курс на {currency_date} {currency_info.find('Name').text}: {currency_info.find('Nominal').text} {currency_info.find('CharCode').text} = {Decimal(currency_info.find('Value').text.replace(',','.'))} руб"
 
